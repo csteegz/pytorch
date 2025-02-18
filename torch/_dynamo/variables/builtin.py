486cf01882d2989ac10bd6274ad529ac8345453c
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-02-18 16:39:20 -0300

    Handle `is`/`is not`
    ghstack-source-id: e52a6fe86b86b11087c8e1fd4bff0a76e04a4886
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146496

--- torch/_dynamo/variables/builtin.py
+++ torch/_dynamo/variables/builtin.py
@@ -660,6 +660,10 @@ class BuiltinVariable(VariableTracker):
                             (VariableTracker, SymNodeVariable),
                             op_var._comparison_with_symnode,
                         ),
+                        (
+                            (variables.ExceptionVariable, variables.ExceptionVariable),
+                            lambda tx, l, r: ConstantVariable(op(l, r))
+                        ),
                     ]
                 )
 
@@ -668,6 +672,8 @@ class BuiltinVariable(VariableTracker):
                     # and True for `is` and `is not`, respectively
                     if type(left) is not type(right):
                         return ConstantVariable.create(op.__name__ != "is_")
+                    if left is right:
+                        return ConstantVariable.create(op(left, right))
 
                 result.append(((VariableTracker, VariableTracker), handle_is))
 
